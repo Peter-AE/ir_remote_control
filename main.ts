fd2aef4c9c2b77c76fd73593d9c713c4010fcfc0
Peter-AE <aap2@gmx.net>
2023-11-14 18:35:47 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,29 @@
 IR.IR_callbackUser(function (message) {
-    if (message == 17) {
+    if (message == 24) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     }
-    if (message == 25) {
+    if (message == 82) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
-    if (message == 20) {
+    if (message == 90) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
-    if (message == 22) {
+    if (message == 8) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     }
-    if (message == 21) {
+    if (message == 28) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
